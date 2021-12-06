57cf92f9b18e76923353ea31058df8001e1d4c15
Farrukh0707 <ashurovfarrux1996@gmail.com>
2021-12-06 23:40:42 +0500

    Create 1-13 darslar.py

---
+++ 1-13 darslar.py
@@ -0,0 +1,369 @@
+#3-DARS
+#print('"Nexia,"', '"Tico,"', "'Damas' ko'rganlar qilar havas")
+#1. 5 ning 4-darajasini topish
+#print('5 ning 4-darajasi', 5**4)
+#2. 22 ni 4 ga bo'lganda qancha qoldiq qoladi ?
+#print('22 ni 4 ga bo\'lganda qoldiq',22%4)
+#3. Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping ?
+#print('Tomonlari 125 ga teng kvadratning yuzi va perimetri','S=',125*125, 'P=',4*125)
+#4. Diametri 12 ga teng bo'lgan doiraning yuzini toping (p=3.14 deb oling)
+#print('Diametri 12 ga teng bo\'lgan doiraning yuzi','S=',3.14*12**2/4)
+#5. Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+#print('Katetlari 6 va 7 bo\'lgan tog\'ri burchakli uchburchakning gipotenuzasi','c=',(6**2+7**2)**(1/2))
+
+#4-DARS
+#boshlash = '"Hello world !"'
+#print(boshlash)
+
+#xabar = "Salom Farrux Ashurov !"
+#print(xabar)
+#xabar = 'Bugun yakshanba dam olish kuni.'
+#print(xabar)
+#xabar="28-noyabr"
+#print(xabar)
+
+#class=23
+#print(class)
+
+#radius = 5
+#pi = 3.14159
+#aylana_yuzi = pi*radius**2
+#print("Radiusi", radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+#5-DARS
+#tuman = "Qarshi"
+#viloyat = "Qashqadaryo"
+#matn = "Men yangi 🚙 sotib oldim."
+#print(matn)
+
+#ism = 'Farrux'
+#familiya = 'Ashurov'
+##print(ism+familiya)
+#print(ism+ ' '+familiya)
+
+#ism = 'stiv'
+#familiya = 'jobs'
+#ism_sharif = f'{ism} {familiya}'
+#print(ism_sharif)
+#print(f'Salom mening ismim {ism}. {ism} {familiya}!')
+#print(f'{ism}:Yangilanish davri keldi! Macintosh yaratiladi!!!')
+
+#print('Hello wolrd')
+#print('Hello \tworld')
+#print('Hello \nworld')
+
+#print(ism_sharif.upper())
+#print(ism_sharif.lower())
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+
+#print('ashurov farrux'.upper())
+#print('ashurov farrux'.capitalize())
+
+#mashina = '      nexia     '
+#print('Men ' + mashina.lstrip() + ' sotib oldim.')
+#print('Men ' + mashina.rstrip() + ' sotib oldim.')
+#print('Men ' + mashina + ' sotib oldim.')
+
+#ism = input('Ismingiz nima ?')
+#print('Assalom alaykum ' + ism)
+
+#yosh = input('Yoshingiz nechada ?')
+#print('Judayam yaxshi. ' + 'Men 25 yoshdaman.')
+
+#kocha = "Bog'bon"
+#mahalla = "Sog'bon"
+#tuman = 'Bodomzor'
+#viloyat = 'Samarqand'
+#print(kocha + " ko'chasi," + " " + mahalla + " mahallasi," + " " + tuman + " tumani," + " " + viloyat + " viloyati")
+
+#ism = input("Ismingiz nima ?")
+#print('Assalom alaykum ' + ism)
+
+#print("Iltimos, quyidagi ma'lumotlarni kiriting:")
+#manzil = input('Viloyatingiz:')
+#manzil1 = input('Tumaningiz:')
+#manzil2 = input('Mahallangiz:')
+#manzil3 = input("Ko'changiz:")
+#print(manzil3 + " ko'chasi, " + manzil2 + " mahallasi, " + manzil1 + " tumani, " + manzil + " viloyati")
+
+#print(kocha + " ko'chasi,\n" + mahalla + " mahallasi,\n" + tuman + " tumani,\n" + viloyat + " viloyati")
+
+#manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+#print(manzil.upper())
+#print(manzil.title())
+#print(manzil.lower())
+#print(manzil.capitalize())
+
+#6-DARS
+#son = int(input('Istalgan son kiriting:'))
+#print(son, 'ning kvadrati', son**2, 'ga teng')
+#print(son, 'ning kubi', son**3, 'ga teng')
+
+#yosh = int(input("Yoshingiz nechada ?"))
+#yil = 2021-yosh
+#print('Siz', yil, "-yilda tug'ilgansiz")
+
+#ism = input("Sizning ismingiz nima ?")
+#print('Assalom alaykum' + ' ' + ism)
+#yosh = int(input("Nechanchi yil tug'ilgansiz ?"))
+#yil = 2021-yosh
+#print('Siz', yil, "yoshdasiz")
+
+#son1 = float(input("Birinchi sonni kiriting:"))
+#son2 = float(input('Ikkinchi sonni kiriting:'))
+#print(f"{son1} + {son2}=", son1+son2)
+#print(f"{son1} - {son2}=", son1-son2)
+#print(f"{son1} * {son2}=", son1*son2)
+#print(f"{son1} / {son2}=", son1/son2)
+
+#7-DARS
+#ismlar = ['Alisher', 'Jahongir', 'Iftixor']
+#print('Salom ' + ismlar[0] + ' ishlaringiz yaxshimi ?')
+#print(f"{ismlar[1]} bugun vaqt bormi ?")
+#print(f"{ismlar[2]} bugun dars soat nechada ?")
+#print('Salom ' + ismlar[0] + ' ishlaringiz yaxshimi ?\n' + f"{ismlar[1]} bugun vaqt bormi ?\n" + f"{ismlar[2]} bugun dars soat nechada ?")
+
+#sonlar = [10, -20, 3, 2.5]
+#print(f"{sonlar[0]} + {sonlar[1]}=", sonlar[0]+sonlar[1])
+#print(f"{sonlar[0]} * {sonlar[1]}=", sonlar[0]*sonlar[1])
+
+#t_shaxslar = ["Alisher Navoiy", "Amir Temur"]
+#z_shaxslar = ["Bil Geyts", "Ilon Mask"]
+#print('Men tarixiy shaxslardan ' + t_shaxslar.pop(0) + ' bilan,\n' 'zamonaviy shaxslardan esa ' + z_shaxslar.pop(1) + ' bilan suhbat qilishni istar edim.')
+
+#friends = []
+#friends.append("Ali")
+#friends.append('Vali')
+#friends.append('Murod')
+#friends.append('Anvar')
+#friends.append('Bobur')
+#print(friends)
+
+#friends.remove('Ali')
+#friends.remove('Vali')
+#print(friends)
+
+#friends.insert(0, 'Farrux')
+#print(friends)
+#friends.insert(2, 'Alisher')
+#print(friends)
+#friends.append('Jahongir')
+#print(friends)
+
+#mehmonlar = []
+#mehmonlar.append(friends.pop(4))
+#mehmonlar.append(friends.pop(3))
+#mehmonlar.append(friends.pop(2))
+#print('Kelgan mehmonlar: ', mehmonlar)
+
+#8-DARS
+#countries = ['Uzbekistan', 'Saudia Arabia', 'Spain', 'France', 'Germany', 'Brasil']
+#print('Mamlakatlar soni: ', len(countries))
+#print(countries)
+#print(sorted(countries))
+#print(sorted(countries, reverse=True))
+#print(countries)
+#countries.reverse()
+#print(countries)
+#countries.sort()
+#print(countries)
+#countries.sort(reverse=True)
+#print(countries)
+#sonlar = list(range(120,1200,2))
+#print(sonlar)
+#print(sum(sonlar))
+#print(f'{max(sonlar)} - {min(sonlar)}=', max(sonlar)-min(sonlar))
+#print('Elementlar soni: ', len(sonlar))
+#print(sonlar[0:20])
+#print(sonlar[260:280])
+#print(sonlar[520:540])
+#taomlar = ['somsa', 'manti', 'osh', 'xonim', 'qozon kabob']
+#nonushta = taomlar[:]
+#taomlar.remove('osh')
+#taomlar.remove('qozon kabob')
+#taomlar.append('sosiska')
+#taomlar.insert(0, 'tuxum')
+#print(taomlar)
+#print(nonushta)
+
+#taomlar = tuple(taomlar)
+#print(taomlar)
+#taomlar[0] = "qaymoq va non"
+
+#9-DARS
+# ismlar = ['Farrux', 'Siroj', 'Iftixor', 'Sunnat', 'Alisher']
+# for ism in ismlar:
+#         print(f'Salom {ism} ! Ishlaringiz yaxshimi ?')
+# print('Kod 5 marta takrorlandi')
+
+# sonlar = list(range(11,100,2))
+# sonlar_kubi = []
+# for son in sonlar:
+#     sonlar_kubi.append(son**3)
+# print(sonlar)
+# print(sonlar_kubi)
+
+# kinolar = []
+# print('5 ta sevimli kinoyingiz qaysilar ?')
+# for k in range(5):
+#         kinolar.append(input(f'{k+1}-kino:'))
+# print(kinolar)
+
+# ismlar = []
+# n_people = int(input('Bugun necha kishi bilan suhbatlashdingiz ?'))
+# for n in range(n_people):
+#     ismlar.append(input(f'{n+1}-suhbatdoshingiz: '))
+# print(ismlar)
+
+# darslar = []
+# ism = input("Ismingiz:")
+# n_dars = int(input("Assalom alaykum " + ism + " bugun nechta fanlardan dars o'tdingiz: "))
+# for n in range(n_dars):
+#     darslar.append(input(f'{n+1}-fan: '))
+# print(darslar)
+
+#10-DARS
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car == 'gm':
+#        print(car.upper())
+#     else:
+#        print(car.title())
+    
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car != 'gm':
+#         print(car.title())
+#     else:
+#         print(car.upper())
+
+# ism = input('Ismingiz:\n')
+# if ism.lower() == 'admin':
+#     print("Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi ?")
+# else:
+#     print(f"Xush kelibsiz, {ism}")
+
+# son = int(input('Istalgan son kiriting:'))
+# if son >=0:
+#     print('Musbat son')
+# else:
+#     print('Manfiy son')
+    
+# son = int(input('Son kiriting:'))
+# if son>0:
+#     print(son**0.5)
+# else:
+#     print('Iltimos, musbat son kiriting!')
+
+#11-DARS
+
+# son = int(input('Juft son kiriting: '))
+# if son%2:
+#     print('Bu son juft emas')
+# else:
+#     print('Rahmat!')
+
+# yosh = int(input('Yoshingiz nechada ? '))
+# if yosh<4 or yosh>60:
+#     print('Sizga muzeyga kirish bepul !')
+# elif yosh<18:
+#     print('Muzeyga kirish narxi 10000 so\'m')
+# elif yosh>18:
+#     print('Muzeyga kirish narxi 20000 so\'m')
+    
+# x = int(input('Birinchi sonni kiriting: '))
+# y = int(input('Ikkinchi sonni kiriting: '))
+# if x==y:
+#     print(f"{x}={y}")
+# elif x<y:
+#     print(f"{x}<{y}")
+# elif x>y:
+#     print(f"{x}>{y}")
+
+# mahsulotlar = ['olma', 'uzum', 'nok', 'banan', 'tarvuz', 'qovun', 'qulupnay', 'ananas', 'kivi', 'apelsin']
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotni qo'shing: "))
+      
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         print(f"Do'konimizda {mahsulot} bor")
+#     else:
+#         print((f"Do'konizmida {mahsulot} yo'q"))
+
+# mahsulotlar = ['olma', 'uzum', 'nok', 'banan', 'tarvuz', 'qovun', 'qulupnay', 'ananas', 'kivi', 'apelsin']
+# savat = []
+# for n in range(5):
+#     savat.append(input(f"Savatga {n+1}-mahsulotani qo'shing: "))
+
+# bor_mahsulotlar = []
+# mavjud_emas = []
+# for mahsulot in savat:
+#     if mahsulot in mahsulotlar:
+#         bor_mahsulotlar.append(mahsulot)
+#     else:
+#         mavjud_emas.append(mahsulot)
+        
+# if mavjud_emas:
+#     print("Do'konimizda quyidagi mahsulotlar yo'q:")
+#     for mahsulot in mavjud_emas:
+#         print(mahsulot)
+# else:
+#     print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+
+# foydalanuvchilar = ['Farrux', 'Alisher', 'Iftixor', 'Jahongir', 'Abror']
+# foydalanuvchi = input('Yangi login tanglang: ')
+
+# if foydalanuvchi.title() not in foydalanuvchilar:
+#         print(f'Xush kelibsiz {foydalanuvchi} !')
+# else:
+#         print('Login band, yangi login tanlang')
+
+# son = int(input('Istalgan butun son kiriting: '))
+# for n in range(2,11):
+#     if not (son%n):
+#         print(f"{son} soni {n} ga qoldiqsiz bo'linadi.")
+
+# 12-DARS
+# Xatolar o'rganilgan.
+
+# 13-DARS
+# GitHub dasturi bilan tanishish
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
