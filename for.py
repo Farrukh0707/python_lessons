a19bf36e51394678a464ce7151113a2d3e767078
Farrukh_Ashurov <95648266+Farrukh0707@users.noreply.github.com>
2023-09-20 18:46:01 +0500

    Add files via upload

---
+++ for.py
@@ -0,0 +1,32 @@
+#MASHQLAR
+#1-mashq
+#ismlar = ['Adham', 'Jamshid', 'Xasan', 'Sodiq', 'Sunnat']
+#size = len(ismlar)
+#for ism in ismlar:
+#    print(f"Salom {ism}")
+#print(f"Kod {size} marta takrorlandi.")
+
+
+#2-mashq
+#toq_sonlar = list(range(11,100,2))
+
+#for toq_son in toq_sonlar:
+#    print(f"{toq_son**3}")
+    
+
+#3-mashq
+#kinolar = []
+#print('5 ta eng sevimli kinolaringiz qaysi ?')
+
+#for n in range(5):
+#        kinolar.append(input(f"{n+1} - kinoyingizni kiriting: "))
+#print(kinolar)
+
+
+#4-mashq
+numb_person = input('Bugun nechta odam bilan suhbatlashdingiz ?\n')
+persons = []
+
+for n in range(int(numb_person)):
+    persons.append(input(f"{n+1} - suhbatdoshingiz ismini yozing: "))
+print(persons)
---
+++ variables.py
@@ -0,0 +1,21 @@
+# MASHQLAR
+# 1-mashq
+
+world = 'Hello world !'
+print(world)
+
+# 2-mashq
+xabar = 'Kuz keldi.'
+print(xabar)
+xabar = "Yoz o'tdi."
+print(xabar)
+
+# 3-mashq
+#class = 'hello'
+#print(class)
+
+# 4-mashq
+radius = 5
+pi = 3.14159
+doira_yuzi = pi * radius**2
+print("Radiusi", radius, "ga teng doiraning yuzi", doira_yuzi, "ga teng.")
