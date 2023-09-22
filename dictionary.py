0166b1c2e0b76ec7ab9170671b053e05ebba3b6f
Farrukh_Ashurov <95648266+Farrukh0707@users.noreply.github.com>
2023-09-22 23:14:37 +0500

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+#MASHQLAR
+#1-mashq
+
+#otam = {'ismi': 'burhonjon', "tugilgan_yili": 1961, "viloyat": "Qashqadaryo"}
+#print(f"Otamning ismi {otam['ismi'].title()}, {otam['tugilgan_yili']}-yilda {otam['viloyat']} viloyatida tug'ilgan.")
+
+
+#2-mashq
+#oila_taom = {'Burhonjon': 'osh', 'Gulnora': 'manti', 'Shahina': 'gulxonim', 'Navfal': 'somsa', 'Farrux': 'qovurilgan baliq'}
+#print(f"Burhonjonning sevimli taomi {oila_taom['Burhonjon']}.")
+#print(f"Gulnoraning sevimli taomi {oila_taom['Gulnora']}.")
+#print(f"Shahinaning sevimli taomi {oila_taom['Shahina']}.")
+#print(f"Navfalning sevimli taomi {oila_taom['Navfal']}.")
+
+#3-mashq
+#python_izohli_lugati = {'integer': 'butun son', 'float': "o'nlik son", 'string': 'matn', 'if': 'agar', 'else': 'yoki', 'for': 'uchun', 'dictionary': 'lug\'at', 'error': 'xato', 'list': 'ro\'yxat', 'while': 'qachonki'}
+#kalit = input('Kalit so\'z kiriting: ').lower()
+#print(python_izohli_lugati.get(kalit, "Bunday so'z mavjud emas."))
+
+#4-mashq
+#tarjima = python_izohli_lugati.get(kalit)
+
+#if tarjima == None:
+#    print("Bunday so'z mavjud emas.")
+#else:
+#    print(f"{kalit.title()} so'zi {tarjima} deb tarjima qilinadi.")
