584e1aafdee595a28410f5bc5945861e78b4049b
Farrukh_Ashurov <95648266+Farrukh0707@users.noreply.github.com>
2023-09-30 20:55:27 +0500

    Create function.py

---
+++ function.py
@@ -0,0 +1,73 @@
+#MASHQLAR
+#1-mashq
+# def yil_hisobla(ism, yosh):
+#     print(f"{ism.title()} {2023-yosh}-yilda tug'ilgansiz.")  
+# yil_hisobla("navfal",30)
+
+
+#2-mashq
+# def son_hisobla(son):
+#     print(f"{son} ning kvadrati - {son**2}; \n"
+#           f"{son} ning kubi - {son**3}.")
+# son_hisobla(30)
+
+
+#3-mashq
+# def juft_toq_aniqla(son):
+#     if son%2 == 0:
+#         print(f"{son} - juft son.")
+#     else:
+#         print(f"{son} - toq son.")
+# juft_toq_aniqla(1966)
+
+
+#4-mashq
+# def katta_son_aniqla(son1, son2):
+#     if son1>son2:
+#         print(f"{son1} - kiritilgan sonlardan kattasi.")
+#     elif son2>son1:
+#         print(f"{son2} - kiritilgan sonlardan kattasi.")
+#     else:
+#         print('Kiritilgan sonlar teng.')
+# katta_son_aniqla(20, 20)
+
+
+#5-mashq
+# def daraja_aniqla(x, y):
+#     print(f"{x} ning {y}-darajasi - {x**y} ga teng.")
+# daraja_aniqla(4, 4)
+
+
+#6-mashq
+# def daraja_aniqla(x, y=2):
+#     print(f"{x} ning {y}-darajasi - {x**y} ga teng.")
+# daraja_aniqla(4)
+
+
+#7-mashq
+# def bolinish_alomatlari(son):
+#     for n in range(2,11):
+#         if son%n==0:
+#             print(f"{son} {n} ga qoldiqsiz bo'linadi.")
+
+# bolinish_alomatlari(70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
